ee8610c22a181b065bcce4c25c57462224aa7270
T45K <tasktas9@gmail.com>
2020-04-16 11:08:00 +0900

    impl Tokenizer.kt

--- src/main/kotlin/io/github/t45k/scm/matching/Tokenizer.kt
+++ src/main/kotlin/io/github/t45k/scm/matching/Tokenizer.kt
@@ -1,5 +1,7 @@
 package io.github.t45k.scm.matching
 
+import io.github.t45k.scm.entity.TokenInformation
+import io.github.t45k.scm.entity.to
 import io.reactivex.Observable
 import io.reactivex.Single
 import org.eclipse.jdt.core.ToolFactory
@@ -7,30 +9,29 @@ import org.eclipse.jdt.core.compiler.IScanner
 import org.eclipse.jdt.core.compiler.ITerminalSymbols
 
 class Tokenizer {
-    fun tokenize(content: String): Single<List<Int>> =
-            Observable.just(content)
-                    .map {
-                        ToolFactory
-                                .createScanner(false, false, true, false)
-                                ?.setSource(it)
-                                ?: emptyScanner()
-                    }
-                    .flatMap { Observable.just(it.nextToken) }
-                    .takeWhile { it != ITerminalSymbols.TokenNameEOF }
-                    .map { normalizeIfNeeded(it) }
-                    .toList()
+    fun tokenize(content: String): Single<List<TokenInformation>> =
+        Observable.just(content)
+            .map {
+                ToolFactory
+                    .createScanner(false, false, true, false)
+                    .setSource(it)
+            }
+            .flatMap { Observable.just(it.nextToken to it.getLineNumber(it.currentTokenStartPosition)) }
+            .takeWhile { it.tokenNumber != ITerminalSymbols.TokenNameEOF }
+            .map { normalizeIfNeeded(it) }
+            .toList()
 
-    private fun normalizeIfNeeded(tokenNumber: Int): Int {
+    private fun normalizeIfNeeded(tokenInformation: TokenInformation): TokenInformation {
         @Suppress("DEPRECATION")
-        return when (tokenNumber) {
-            ITerminalSymbols.TokenNameIdentifier -> 1000
+        return when (tokenInformation.tokenNumber) {
+            ITerminalSymbols.TokenNameIdentifier -> 1000 to tokenInformation.lineNumber
             ITerminalSymbols.TokenNameCharacterLiteral,
             ITerminalSymbols.TokenNameDoubleLiteral,
             ITerminalSymbols.TokenNameFloatingPointLiteral,
             ITerminalSymbols.TokenNameIntegerLiteral,
             ITerminalSymbols.TokenNameLongLiteral,
-            ITerminalSymbols.TokenNameStringLiteral -> 1001
-            else -> tokenNumber
+            ITerminalSymbols.TokenNameStringLiteral -> 1001 to tokenInformation.lineNumber
+            else -> tokenInformation
         }
     }
 
@@ -38,8 +39,4 @@ class Tokenizer {
         this.source = source.toCharArray()
         return this
     }
-
-    private fun emptyScanner(): IScanner =
-            ToolFactory.createScanner(false, false, true, false).setSource("")
-
 }
