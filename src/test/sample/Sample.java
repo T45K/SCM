b0dfff97e93fd1a7811afcdb45ef00cb99532ebf
T45K <tasktas9@gmail.com>
2020-04-18 00:49:18 +0900

    add test

--- src/test/sample/Sample.java
+++ src/test/sample/Sample.java
@@ -16,4 +16,14 @@ class Sample {
     public static int min(final int a, final int b) {
         return a < b ? a : b;
     }
+
+    public static void defineLiterals() {
+        int a = 0;
+        short b = 0;
+        long c = 0l;
+        float d = 0f;
+        double e = 0d;
+        char f = '0';
+        String g = "0";
+    }
 }
