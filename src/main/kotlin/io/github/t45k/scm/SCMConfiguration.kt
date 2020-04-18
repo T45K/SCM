eb2b3ad297ac9252915323e9e4c683737b656cdd
T45K <tasktas9@gmail.com>
2020-04-18 22:39:04 +0900

    impl

---
+++ src/main/kotlin/io/github/t45k/scm/SCMConfiguration.kt
@@ -0,0 +1,43 @@
+package io.github.t45k.scm
+
+import org.kohsuke.args4j.CmdLineException
+import org.kohsuke.args4j.CmdLineParser
+import org.kohsuke.args4j.Option
+import org.kohsuke.args4j.OptionDef
+import org.kohsuke.args4j.spi.Messages
+import org.kohsuke.args4j.spi.OptionHandler
+import org.kohsuke.args4j.spi.Parameters
+import org.kohsuke.args4j.spi.PathOptionHandler
+import org.kohsuke.args4j.spi.Setter
+import java.nio.file.Path
+
+
+class SCMConfiguration {
+    @Option(name = "-i", aliases = ["--input-dir"], usage = "Input directory", handler = PathOptionHandler::class)
+    lateinit var inputDir: Path
+
+    @Option(name = "-s", aliases = ["--source-file"], usage = "Single source file", handler = PathOptionHandler::class)
+    lateinit var sourceFile: Path
+
+    @Option(name = "-q", aliases = ["--query"], usage = "Query", required = true, handler = QueryHandler::class)
+    lateinit var query: Array<String>
+
+    fun isInputDirInitialized(): Boolean = ::inputDir.isInitialized
+    fun isSourceFileInitialized(): Boolean = ::sourceFile.isInitialized
+}
+
+class QueryHandler(parser: CmdLineParser, option: OptionDef, setter: Setter<String>)
+    : OptionHandler<String>(parser, option, setter) {
+    override fun getDefaultMetaVariable(): String {
+        return Messages.DEFAULT_META_STRING_ARRAY_OPTION_HANDLER.format()
+    }
+
+    @Throws(CmdLineException::class)
+    override fun parseArguments(params: Parameters): Int {
+        (0 until params.size())
+            .map { params.getParameter(it) }
+            .forEach(setter::addValue)
+
+        return params.size()
+    }
+}
