2370605e3ca2624d0deaab08e75365269aab6953
T45K <tasktas9@gmail.com>
2020-04-30 22:48:09 +0900

    update

--- src/main/kotlin/io/github/t45k/scm/matching/algorithms/suffixArray/DoublingConstructor.kt
+++ src/main/kotlin/io/github/t45k/scm/matching/algorithms/suffixArray/DoublingConstructor.kt
@@ -10,28 +10,31 @@ class DoublingConstructor : ArrayConstructor {
         var rank: Array<Int> = Array(size) { from[it] }
         var k = 1
 
-        val compareWithRank: Comparator<Int> = Comparator { i, j ->
-            if (rank[i] != rank[j]) {
-                rank[i] - rank[j]
-            } else {
-                val iWithRank = if (i + k < size) rank[i + k] else -1
-                val jWithRank = if (j + k < size) rank[j + k] else -1
-                iWithRank - jWithRank
-            }
+        val compareByRank: Comparator<Int> = Comparator<Int> { i, j ->
+            rank[i] - rank[j]
+        }.thenComparing { i, j ->
+            val iWithRank = if (i + k < size) rank[i + k] else -1
+            val jWithRank = if (j + k < size) rank[j + k] else -1
+            iWithRank - jWithRank
         }
 
         while (k <= size) {
-            sortedIndices.sortWith(compareWithRank)
-            val tmp: Array<Int> = Array(size) { it }
-            tmp[sortedIndices[0]] = 0
-            for (i in 1 until size) {
-                tmp[sortedIndices[i]] = tmp[sortedIndices[i - 1]] + if (compareWithRank.compare(sortedIndices[i - 1], sortedIndices[i]) >= 0) 0 else 1
-            }
-            rank = tmp
+            sortedIndices.sortWith(compareByRank)
+            rank = updateRank(sortedIndices, compareByRank, size)
             k *= 2
         }
 
-        return sortedIndices.map { it to from.subList(it, from.size) }
+        return sortedIndices
+            .map { it to from.subList(it, from.size) }
             .toList()
+    }
+
+    private fun updateRank(sortedIndices: Array<Int>, compareByRank: Comparator<Int>, size: Int): Array<Int> {
+        val tmp: Array<Int> = Array(size) { 0 }
+        tmp[sortedIndices[0]] = 0
+        for (i in 1 until size) {
+            tmp[sortedIndices[i]] = tmp[sortedIndices[i - 1]] + if (compareByRank.compare(sortedIndices[i - 1], sortedIndices[i]) >= 0) 0 else 1
+        }
+        return tmp
     }
 }
