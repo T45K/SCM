c24c7c29d31c8ae0e157f89ce25a4ad6ff262801
T45K <tasktas9@gmail.com>
2020-04-30 23:04:10 +0900

    modify test

--- src/test/kotlin/io/github/t45k/scm/matching/algorithms/SuffixArraySearchTest.kt
+++ src/test/kotlin/io/github/t45k/scm/matching/algorithms/SuffixArraySearchTest.kt
@@ -1,21 +1,33 @@
 package io.github.t45k.scm.matching.algorithms
 
 import io.github.t45k.scm.matching.JDTTokenizer
+import io.github.t45k.scm.matching.algorithms.suffixArray.NaiveConstructor
 import io.github.t45k.scm.matching.algorithms.suffixArray.SuffixArraySearch
 import org.junit.Test
 import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.test.assertEquals
 
-
 internal class SuffixArraySearchTest {
     @Test
-    fun testSearch() {
+    fun testSearchWithDoubling() {
         val query: List<Int> = JDTTokenizer().tokenize("a>b?a:b;")
             .map { it.tokenNumber }
         val from: List<Int> = JDTTokenizer().tokenize(Files.readString(Paths.get("./src/test/sample/Sample.java")))
             .map { it.tokenNumber }
         val searchedResult: List<Int> = SuffixArraySearch(query).search(from)
+        assertEquals(2, searchedResult.size)
+        assertEquals(42, searchedResult[0])
+        assertEquals(18, searchedResult[1])
+    }
+
+    @Test
+    fun testSearchWithNaive() {
+        val query: List<Int> = JDTTokenizer().tokenize("a>b?a:b;")
+            .map { it.tokenNumber }
+        val from: List<Int> = JDTTokenizer().tokenize(Files.readString(Paths.get("./src/test/sample/Sample.java")))
+            .map { it.tokenNumber }
+        val searchedResult: List<Int> = SuffixArraySearch(query, NaiveConstructor()).search(from)
         assertEquals(2, searchedResult.size)
         assertEquals(42, searchedResult[0])
         assertEquals(18, searchedResult[1])
